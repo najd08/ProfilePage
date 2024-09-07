9793d3947148ebe5075c5640e3930c6800bf42ce
najd08 <120324320+najd08@users.noreply.github.com>
2024-09-07 19:36:11 +0300

    Initial Commit

---
+++ ProfilePage/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ProfilePage
+//
+//  Created by Najd Alsabi on 07/09/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
