21c44fc2d431d99fa72dddce3bccb431fe98b72f
najd08 <120324320+najd08@users.noreply.github.com>
2024-09-10 18:27:38 +0300

    updated

---
+++ ProfilePage/SignUpPage.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpPage.swift
+//  ProfilePage
+//
+//  Created by Najd Alsabi on 07/09/2024.
+//
+
+import SwiftUI
+
+struct SignUpPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpPage()
+}
